0d97256d9d4a79dbf7e92f3c02437ee6235410fb
socialclimber <newtnnkeng@gmail.com>
2019-02-18 09:44:15 +0200

    Simple example to declare a variable assign a value to it and print it on the screen

---
+++ Animal.java
@@ -0,0 +1,14 @@
+
+// Declare the java class
+public class Animal{
+
+	// add main method to the program
+	public static void main(String[] args){
+
+		// Declare a variable and store the name in it
+		String name = "Lion";
+
+		//print out the value of the variable
+		System.out.println(name);
+	}
+}
---
+++ Exercise1/Example.java
@@ -0,0 +1,6 @@
+public class Animal{
+	public static void main(String[] args){
+		String name = "Lion";
+		System.out.println(name);
+	}
+}
