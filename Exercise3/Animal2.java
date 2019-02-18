7c8db59b61b39c75dc1e6834aadda7aa7ebc775f
socialclimber <newtnnkeng@gmail.com>
2019-02-18 11:15:11 +0200

    an example to print the information of an animal

---
+++ Exercise3/Animal2.java
@@ -0,0 +1,13 @@
+// declare the class
+public class Animal2{
+// add the main method
+	public static void main(String[] args){
+		// declare a variable and save the name in it
+		String name = "Lion";
+		// delcare another variable and save the ave in it
+		int age = 24;
+
+		// output the animals information on the screen
+		System.out.println("This Animal "+name+" has as age "+age);
+	}
+}
