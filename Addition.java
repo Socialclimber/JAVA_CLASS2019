4ce0a8b8410a7764c10cfcd7436fa1b57fa7933f
socialclimber <newtnnkeng@gmail.com>
2019-02-18 10:59:16 +0200

    Just updated the addition example

---
+++ Addition.java
@@ -0,0 +1,14 @@
+// declare the java class
+public class Addition{
+	// add the main method
+	public static void main(String[] args){
+		// declare variables and assign values to them
+		int a = 10;
+		int b = 15;
+		// perform the addition operation and save the result in another variable c
+		int c = a + b;
+		// display the results on the screen .
+		System.out.println("The sum of a + b is "+c);
+
+	}
+}
