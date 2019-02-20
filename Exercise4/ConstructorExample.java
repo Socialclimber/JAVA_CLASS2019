f92125e31a591d00482e61dcebe82f1979e67acd
socialclimber <newtnnkeng@gmail.com>
2019-02-20 13:20:37 +0200

    Hello world example that uses constructors

---
+++ Exercise4/ConstructorExample.java
@@ -0,0 +1,10 @@
+public class ConstructorExample{
+	
+	public ConstructorExample(){
+		System.out.println("Hello World! ");
+	}
+
+	public static void main(String[] args){
+		new ConstructorExample();
+	}
+}
